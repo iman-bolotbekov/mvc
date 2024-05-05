41474929b65e634256d322e0c5ddc709df38fd0a
iman-bolotbekov <iman114@gmail.com>
2024-05-05 12:33:04 +0600

    first commit

---
+++ src/main/java/web/models/Car.java
@@ -0,0 +1,61 @@
+package web.models;
+
+import java.util.Objects;
+
+public class Car {
+    private Integer id;
+    private String model;
+    private String color;
+    public Car() {}
+    public Car(Integer id, String model, String color) {
+        this.id = id;
+        this.model = model;
+        this.color = color;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Car car = (Car) o;
+        return Objects.equals(id, car.id) && Objects.equals(model, car.model) && Objects.equals(color, car.color);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, model, color);
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "id=" + id +
+                ", model='" + model + '\'' +
+                ", color='" + color + '\'' +
+                '}';
+    }
+}
